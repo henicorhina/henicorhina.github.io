95e4272ebfbb004eb23851fd3dedbf7c09097fd0
henicorhina <henicorhina@gmail.com>
2026-05-07 21:12:57 +0000

    Deploying to gh-pages from @ henicorhina/henicorhina.github.io@7ff3668b0f5f075d3c8859443be67db2be6d6072 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,9 +435,9 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-great-white-heron",
+          title: 'Great White Heron',
+          description: "Speciation in the Great Blue / Great White Heron complex",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
