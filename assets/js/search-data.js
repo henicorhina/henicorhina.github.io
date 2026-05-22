339f7c5340661640f3fc31b825ec90d46299e458
henicorhina <henicorhina@gmail.com>
2026-05-22 00:25:47 +0000

    Deploying to gh-pages from @ henicorhina/henicorhina.github.io@53de2a8ebc58bef7d725ecdccf9d4213274500cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "People",
+        },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
