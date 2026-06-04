e4a5da7731c7f43ab154f5f581e3045d971546ab
henicorhina <henicorhina@gmail.com>
2026-06-04 00:59:36 +0000

    Deploying to gh-pages from @ henicorhina/henicorhina.github.io@0a5675a0b5a917eed2ac117c17f3295ed119fa29 🚀

--- assets/js/photoswipe-setup.js
+++ assets/js/photoswipe-setup.js
@@ -1,8 +1,32 @@
 import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js";
 import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js";
-const photoswipe = new PhotoSwipeLightbox({
+
+const lightbox = new PhotoSwipeLightbox({
   gallery: ".pswp-gallery",
   children: "a",
   pswpModule: PhotoSwipe,
 });
-photoswipe.init();
+
+lightbox.on('uiRegister', function() {
+  lightbox.pswp.ui.registerElement({
+    name: 'caption',
+    order: 9,
+    isButton: false,
+    appendTo: 'root',
+    html: '',
+    onInit: (el, pswp) => {
+      lightbox.pswp.on('change', () => {
+        const currSlideElement = lightbox.pswp.currSlide.data.element;
+        let captionHTML = '';
+        if (currSlideElement) {
+          captionHTML = currSlideElement.querySelector('img').getAttribute('alt') || '';
+          const dataCap = currSlideElement.getAttribute('data-pswp-caption');
+          if (dataCap) captionHTML = dataCap;
+        }
+        el.innerHTML = captionHTML;
+      });
+    }
+  });
+});
+
+lightbox.init();
